cb381319614bebac3e7e48291b4f9e6c65666ecd
Owen <rebellerowen@gmail.com>
2023-09-25 00:19:06 +0200

    ajout communes

--- script/script.js
+++ script/script.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    function newsLetter() {
-        let mail = document.getElementById("input-news");
-        let btn = document.getElementById("btn-news");
-
-        btn.addEventListener("click", () => {
-            mail.value = "";
-        });
-    }
-
     newsLetter();
+    savoirPlus();
 });
+
+function newsLetter() {
+    let mail = document.getElementById("input-news");
+    let btn = document.getElementById("btn-news");
+
+    btn.addEventListener("click", () => {
+        mail.value = "";
+    });
+}
+
+function savoirPlus() {
+    let btn = document.getElementById("btn");
+    let cadre = document.querySelector(".description-commune")
+
+    btn.addEventListener("click", () => {
+        cadre.style.overflowY = "auto"
+        btn.style.display = "none";
+    });
+}
