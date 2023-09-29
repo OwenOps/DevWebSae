51ca7059108ce75626b45a5d7f9348ffcd7f1379
Owen <rebellerowen@gmail.com>
2023-09-29 23:39:17 +0200

    finis

--- script/accueil.js
+++ script/accueil.js
@@ -2,19 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     init()
 });
 
-var clics;
 var ancienChoix;
-var btn;
 var cadre;
 
 function init() {
-    clics = 0;
     ancienChoix = "commune";
-    btnD = document.getElementById("btn");
     cadre = document.getElementById("description-commune");
 
     newsLetter();
-    savoirChoix();
     afficheDescription();
 }
 
@@ -27,45 +22,8 @@ function newsLetter() {
     });
 }
 
-function savoirPlus() {
-    cadre.style.overflowY = "auto";
-    cadre.appendChild(btnD)
-    btnD.style.marginBottom = "15px"
-    btnD.querySelector("span").textContent = "Voir moins"
-
-    document.getElementById("img-fleche").src = "../img/fleche-haut.png"
-}
-
-function savoirMoins() {
-    let boiteBtn = document.getElementById("boite-bouton")
-
-    cadre.style.overflowY = "hidden"; 
-    cadre.scrollTop = 0; 
-    boiteBtn.appendChild(btnD);
-    btnD.querySelector("span").textContent = "En savoir plus";
-
-    document.getElementById("img-fleche").src = "../img/fleche-bas.png"
-}
-
-function savoirChoix() {
-    btnD.addEventListener("click", () => {
-        clics++;
-        if (clics % 2 == 1) {
-            savoirPlus();
-        } 
-        else 
-        {
-            savoirMoins();
-        }
-    })
-}
-
 function estValeurCommune(nouveauChoix) {
     return nouveauChoix == document.getElementById("choix-agglo").options[0].value;
-}
-
-function pasClique() {
-    return !(document.getElementById("boite-bouton").contains(btnD))
 }
 
 function afficheDescription() {
@@ -73,15 +31,6 @@ function afficheDescription() {
 
     choix.addEventListener("change", () => {
         let nouveauChoix = choix.options[choix.selectedIndex].value;
-
-        //Si la personne ne clique pas sur le bouton et qu'il change directement de valeur
-        if (pasClique())
-            clics++;
-
-        if (estValeurCommune(nouveauChoix))
-            btnD.style.display = "none";
-        else 
-            btnD.style.display = "block";
 
         // Masquez les anciennes valeurs.
         let anciennesValeurs = document.getElementsByClassName(ancienChoix);
@@ -94,7 +43,7 @@ function afficheDescription() {
             nouvellesValeurs[i].style.display = "block";
         }
         
+        cadre.scrollTop = 0;;
         ancienChoix = nouveauChoix;
-        savoirMoins();
     });
 }
