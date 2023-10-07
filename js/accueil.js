c957b5848d701c0f8b96e508623fa0675f6be8ea
Owen <rebellerowen@gmail.com>
2023-10-07 16:58:31 +0200

    init

--- js/accueil.js
+++ js/accueil.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+    init();
     newsLetter();
     counter();
+    typewriter();
 });
+var title;
+var txt;
+
+function init() {
+    title = document.querySelector('h1');
+    txt = "Bienvenue à Chartres Métropole !";
+}
 
 function newsLetter() {
     let mail = document.getElementById("input-news");
@@ -17,3 +26,16 @@ function counter() {
         time: 2000,
     });
 }
+
+function typewriter(word, index){
+    if(index < word.length) {
+        setTimeout(() => {
+            title.innerHTML += `<span>${word[index]}</span>`
+            typewriter(txt, index + 1)
+        }, 100);
+    }
+}
+
+setTimeout(() => {
+    typewriter(txt, 0)
+}, 500);
